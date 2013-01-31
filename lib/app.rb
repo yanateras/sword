1b44cdc30715b8fdc728518850bc1eb283cd688e
Yegor Timoshenko <yegortimoshenko@riseup.net>
2013-01-31 17:41:00 +0400

    Be agnostic from project's structure

--- lib/app.rb
+++ lib/app.rb
@@ -15,47 +15,40 @@ require 'sinatra/base'
 
 class Sword < Sinatra::Base
   def self.version; "0.1.0" end
-  set :scripts, 'scripts'
-  set :styles, 'styles'
-  set :views, 'templates'
-  set :public_folder, 'other'
+  set :views, '.'
+  set :public_folder, '.'
   set :port, 1111
 
   get '/favicon.ico' do
     send_file File.dirname(__FILE__) + "/../favicon.ico"
   end
 
-  get '/*.css' do |css|
+  get '/*.css' do |style|
     content_type 'text/css', charset: 'utf-8'
     # If there is a CSS, take it.
     # Otherwise, use SASS.
-    style = "#{settings.styles}/#{css}"
     return send_file "#{style}.css" if File.exists? "#{style}.css"
     return less style.to_sym if File.exists? "#{style}.less"
     # SASS/Compass
-    sass css.to_sym, Compass.sass_engine_options
-      .merge(views: settings.styles, style: :compressed)
+    sass style.to_sym, Compass.sass_engine_options
+      .merge(style: :compressed)
   end
 
-  get '/*.js' do |js|
+  get '/*.js' do |script|
     content_type 'application/x-javascript', charset: 'utf-8'
     # If there is a JS, take it.
     # Otherwise, use CoffeeScript.
-    script = "#{settings.scripts}/#{js}.js"
     return send_file script if File.exists? script
-    coffee js.to_sym, views: settings.scripts
+    coffee js.to_sym
   end
 
   get '/' do
     redirect '/index'
   end
 
-  # Slim & HTML
-  get '/*/?' do |html|
-    template = "#{settings.views}/#{html}"
-    # Make HTML in `templates` folder possible.
-    return File.read "#{template}.html" if File.exists? "#{template}.html"
-    return haml page.to_sym if File.exists? "#{template}.haml"
-    slim html.to_sym
+  get '/*/?' do |page|
+    return File.read "#{page}.html" if File.exists? "#{page}.html"
+    return haml page.to_sym if File.exists? "#{page}.haml"
+    return slim page.to_sym if File.exists? "#{page}.slim"
   end
 end
