0b93003d6d365c5df277bcefd8e401d545a59dd9
Yegor Timoshenko <yegortimoshenko@riseup.net>
2013-02-01 19:00:34 +0400

    Resolve issues with Compass

--- lib/app.rb
+++ lib/app.rb
@@ -1,32 +1,25 @@
 require 'sinatra/base'
-require 'psych'
 
 class Sword < Sinatra::Base
-  def self.version; "0.2.0" end
-  dir = File.dirname(__FILE__)
-  engine = Psych.load_file "#{dir}/engine.yml"
-  # Hook-up all gems that we will
-  # probably need; open an issue
-  # if this list is missing smth.
-  engine['gems'].each do |g|
-    begin require g
-    rescue LoadError; next end
-  end
-
-  engine['markdown'].each do |m|
-    begin require m; break
-    rescue LoadError; next end
+  def self.version; "0.2.1" end
+  dir = File.dirname __FILE__
+  sassy = {cache: false}; if defined? Compass
+    # If Compass is defined, than use the configuration
+    # file and inject all the settings into stylesheet
+    # hash called `sassy`
+    Compass.add_project_configuration "#{dir}/compass.rb"
+    sassy = Compass.sass_engine_options
   end
 
   disable :show_exceptions
 
   error do
     @error = env['sinatra.error']
-    erb :error, views: "#{dir}/../"
+    erb :error, views: dir
   end
 
   get '/favicon.ico' do
-    send_file "#{dir}/../favicon.ico"
+    send_file "#{dir}/favicon.ico"
   end
 
   set :views, '.'
@@ -35,22 +28,23 @@ class Sword < Sinatra::Base
 
   get '/*.css' do |style|
     return send_file "#{style}.css" if File.exists? "#{style}.css"
-    engine['styles'].each do |k,v| v.each do |e| # for extension
-      return send k, style.to_sym, Compass.sass_engine_options
-        .merge(line_comments: false, cache: false) if File.exists? "#{style}.#{e}"
+    $engine['styles'].each do |k,v| v.each do |e| # for extension
+      # For every extension there is a stylesheet engine (SASS e.g.)
+      return send k, style.to_sym, sassy if File.exists? "#{style}.#{e}"
     end; end
     raise "Stylesheet not found"
   end
 
   get '/*.js' do |script|
     return send_file "#{script}.js" if File.exists? "#{script}.js"
-    engine['scripts'].each do |k,v| v.each do |e| # for extension
+    $engine['scripts'].each do |k,v| v.each do |e| # for extension
       return send k, script.to_sym if File.exists? "#{script}.#{e}"
     end; end
     raise "Script not found"
   end
 
   get '/' do
+    # Call /index, the same shit
     call env.merge('PATH_INFO' => "/index")
   end
 
@@ -60,7 +54,8 @@ class Sword < Sinatra::Base
       # If you know another ultra-dumbass html extension, let me know.
       return send_file "#{page}.#{e}" if File.exists? "#{page}.#{e}"
     end
-    engine['pages'].each do |k,v| v.each do |e| # for extension
+    $engine['pages'].each do |k,v| v.each do |e| # for extension
+      # If Slim, then prettify the code so it is OK to read
       return send k, page.to_sym, pretty: true if File.exists? "#{page}.#{e}"
     end; end
     # Is it an index? Call it recursively.
---
+++ lib/compass.rb
@@ -0,0 +1,12 @@
+output_style = :nested
+line_comments = false
+relative_assets = true
+asset_cache_buster :none
+cache = false
+
+sass_dir = '.'
+images_dir = sass_dir
+http_images_path = sass_dir
+
+http_path = '/'
+http_stylesheets_path = http_path
