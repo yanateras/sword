dc91fc69e5f99dea8fb8b00c58a8340a3e5440fb
Yegor Timoshenko <yegortimoshenko@riseup.net>
2013-06-03 15:19:20 +0400

    Add jQuery helper, extract HTML formats in parsing.yml, refactor

--- lib/main.rb
+++ lib/main.rb
@@ -7,27 +7,24 @@ module Sword
   PARSING  = YAML.load_file "#{LIBRARY}/parsing.yml"
   VERSION  = '0.7.1'
 
-  # Hook-up all gems that we will probably need:
-  PARSING['gems'].concat(File.exists?(REQUIRED) ? File.read(REQUIRED).split("\n") : []).each do |lib|
-    Hash === lib ? lib.values.first.each { |g| begin require g; break; rescue LoadError; next end } :
-    begin require lib; rescue LoadError; end
-  end
-
   class Application < Sinatra::Base
     # This piece of code is from Sinatra,
     # tweaked a bit to silent Thin server
     # and add Sword version and &c.
     NotFound = Class.new StandardError
     class << self
-      def run! options = {}
+      def run!(options = {})
+        load
         options = {:debug => false, :directory => Dir.pwd, :port => 1111, :silent => false}.merge(options)
         server_settings = settings.respond_to?(:server_settings) ? settings.server_settings : {}
         detect_rack_handler.run self, server_settings.merge(Port: options[:port], Host: bind) do |server|
-          STDERR.print ">> Sword #{VERSION} at your service!\n" +
-          "   http://localhost:#{options[:port]} to see your project.\n" +
-          "   CTRL+C to stop.\n"
           [:INT, :TERM].each { |s| trap(s) { quit!(server) } }
-          STDERR.print options.map { |k,v| "## #{k.capitalize}: #{v}\n"}.inject { |sum, n| sum + n } if options[:debug] and not options[:silent]
+          STDERR.print ">> Sword #{VERSION} at your service!\n" \
+          "   http://localhost:#{options[:port]} to see your project.\n" \
+          "   CTRL+C to stop.\n"
+          STDERR.print options.
+            map { |k,v| "## #{k.capitalize}: #{v}\n" }.
+            inject { |sum, n| sum + n } if options[:debug] and not options[:silent]
           unless options[:debug]
             server.silent = true
             disable :show_exceptions
@@ -40,6 +37,18 @@ module Sword
         end
       rescue Errno::EADDRINUSE, RuntimeError
         STDERR.print "!! Port is in use. Is Sword already running?\n"
+      end
+      def load
+        # Hook-up all gems that we will probably need:
+        PARSING['gems'].concat(File.exists?(REQUIRED) ? File.read(REQUIRED).split("\n") : []).each do |lib|
+          Hash === lib ? lib.values.first.each { |g| begin require g; break; rescue LoadError; next end } :
+          begin require lib; p lib; rescue LoadError; p lib + 'not' end
+        end
+
+        if defined? Compass
+          Compass.add_project_configuration "#{LIBRARY}/compass.rb"
+          @compass = Compass.sass_engine_options
+        end
       end
       def quit!(server)
         STDERR.print "\n"
@@ -58,9 +67,12 @@ module Sword
       end end end
     end
 
-    if defined? Compass
-      Compass.add_project_configuration "#{LIBRARY}/compass.rb"
-      compass = Compass.sass_engine_options
+    helpers do
+      def jquery(version = '1.8.3')
+        "<script src='//ajax.googleapis.com/ajax/libs/jquery/#{version}/jquery.min.js'/>"
+      end
+      def font(options)
+      end
     end
 
     error do
@@ -77,16 +89,16 @@ module Sword
       call env.merge 'PATH_INFO' => '/index'
     end
 
-    parse 'styles', '/*.css', (compass || {})
+    parse 'styles', '/*.css', (@compass || {})
     parse 'scripts', '/*.js'
 
-    get '*.html?' do |route|
+    get %r{(.+?)\.(#{PARSING['html'] * '|'})} do |route, _|
       call env.merge 'PATH_INFO' => route
     end
 
     set :slim, :pretty => true
     parse 'pages', '/*/?' do |page|
-      %w[html htm xhtml xht dhtml dhtm].each do |extension|
+      PARSING['html'].each do |extension|
         # If you know another ultra-dumbass html extension, let me know.
         return send_file "#{page}.#{extension}" if File.exist? "#{page}.#{extension}"
       end
