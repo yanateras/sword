fe8f526c7a8f3a5ad0dbc6c59359025d7cf285dc
Yegor Timoshenko <yegortimoshenko@riseup.net>
2013-06-12 03:43:17 +0400

    Implement index.html fetch test

--- test/running.rb
+++ test/running.rb
@@ -5,4 +5,8 @@ describe 'running and loading' do
   it 'prints help information' do
     assert `bin/sword -h`['Usage: sword [options]']
   end
+  it 'gets index.html' do
+    `bin/sword -d example`
+    assert_equal `curl localhost:1111`, File.read('example/index.html')
+  end
 end
