46660fa1d775e6bed15bc576731c96f26e435de0
Yegor Timoshenko <yegortimoshenko@riseup.net>
2013-06-18 22:32:05 +0400

    Add Fotorama mixin

--- lib/sword.rb
+++ lib/sword.rb
@@ -143,8 +143,22 @@ module Sword
       def init
         helpers do
           def font(options) end
+          
           def jquery(version = '1.8.3')
             "<script src='//ajax.googleapis.com/ajax/libs/jquery/#{version}/jquery.min.js'/>"
+          end
+
+          # Fotorama
+          def fotorama_css(version = '4.1.5')
+            "<link rel='stylesheet' href='//fotorama.s3.amazonaws.com/#{version}/fotorama.css'/>"
+          end
+
+          def fotorama_js(version = '4.1.5')
+            "<script src='//fotorama.s3.amazonaws.com/#{version}/fotorama.js'></script>"
+          end
+
+          def fotorama(version = '4.1.5')
+            fotorama_css(version) + fotorama_js(version)
           end
         end
 
